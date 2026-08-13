816a0ce8f7183f44ac22433498b1af1895296bd7
Adrian27007 <a.olarteb2@uniandes.edu.co>
2026-08-12 22:26:49 -0500

    modificaciones por debug lab

--- App/view.py
+++ App/view.py
@@ -57,9 +57,7 @@ def print_menu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Mods Lab 1, agregar la opcion 3.
-    # Agregue la opción 3 para cargar los tags de los libros.
-    # Pueede guiarse de las opciones 1 y 2.
+    print("3- Cargar Tags de los libros")
     print("0- Salir")
 
 
@@ -124,23 +122,10 @@ def main():
             books = load_books(app)
             print("Total de libros cargados: " + str(books) + "\n")
 
-        elif int(inputs[0]) == 2:
-            print("Cargando información de tags....")
-            tags = load_tags(app)
-            print("Total de tags cargados: " + str(tags) + "\n")
-
-        # TODO: Mods Lab 1, agregar la funcion opt 3 -> load_book_tags().
-        # Agregue la opción 3 que llama a la función load_books_tags() (creada en la lógica).
-        # Esta función carga los tags de los libros en el catalogo.
-        # Puede guiarse de las opciones 1 y 2.
-        # Imprima el total de tags de los libros cargados.
+        elif int(inputs[0]) == 3:
+                print("Cargando información de tags de los libros....")
+                booksTags = load_books_tags(app)
+                print("Total de tags de los libros cargados: " + str(booksTags) +
+    "\n")
         elif int(inputs[0]) == 3:
             pass
-
-        elif int(inputs[0]) == 0:
-            working = False
-            print("\nGracias por utilizar el programa.")
-
-        else:
-            print("Opcion erronea, vuelva a elegir.\n")
-    sys.exit(0)
